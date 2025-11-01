caebac487eb3821de3153374e914840e93a648df
MITU-DK <mitudk2441@gmail.com>
2025-11-01 07:43:03 +0530

    add header

---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>choosing from a diverse menu featuring a detectable array of dishes crafted with the finest ingridents and culinary expertise. Our mission is satisy you craving and elevate your dining experince, onedelicious meal at time. </p>
+            <button>View Menu</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
