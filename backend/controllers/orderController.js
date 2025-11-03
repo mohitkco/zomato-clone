5ebe0e900190568df3629cbf378db896bcf67419
mohitkco <mohitsarraf107@gmail.com>
2025-11-03 23:14:51 +0530

    nothing

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -59,4 +59,20 @@ const placeOrder = async(req, res) => {
 
 
 }
-export { placeOrder }
+
+// api for updating order status
+const updateStatus = async (req,res) => {
+try {
+    await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+    res.json({success:true,message:"Status Updated"})
+} catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+}
+
+}
+
+
+
+
+export { placeOrder,verifyOrder,userOrder,listOrders,updateStatus }
